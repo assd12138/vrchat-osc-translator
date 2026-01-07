b738542173623101327d1be857ecea44f9f728a8
yaozhen <yaozhen@wuji.pro>
2026-01-07 10:53:56 +0800

    init: 初始化安卓

---
+++ src-tauri/gen/android/app/src/main/java/com/apple/vrchat_osc_tauri/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.apple.vrchat_osc_tauri
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { invoke } from '@tauri-apps/api/core';
 import './App.css'
 import Audiopanel from './panel/audiopanel';
+
+async (name: string)=>{
+  const a = await invoke<string>("greet", { name })
+  console.log(a);
+}
 function App() {
 
   return (
