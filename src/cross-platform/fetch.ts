c3db112057cf9d48d94a3a4d14ecda64b0d425ef
yaozhen <1796684119@qq.com>
2026-05-09 21:36:28 +0800

    fix:解决--disable-web-security导致的tauri异常问题

--- src/cross-platform/fetch.ts
+++ src/cross-platform/fetch.ts
@@ -1,1 +1,16 @@
-export default fetch;
+import {
+  type ClientOptions,
+  fetch as tauriFetch,
+} from "@tauri-apps/plugin-http";
+import { RUNTIME, runtime } from "./environmentDetect";
+
+export default function (
+  input: string | URL | Request,
+  init?: (RequestInit & ClientOptions) | undefined,
+) {
+  if (runtime === RUNTIME.TAURI) {
+    return tauriFetch(input, init);
+  } else {
+    return fetch(input, init);
+  }
+}
